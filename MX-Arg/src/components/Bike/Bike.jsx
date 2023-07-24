3587b89fc1547a639f0a2fb3929b33e301898043
Tomás Arrativel <arrativeltomas@gmail.com>
2023-07-24 10:42:11 -0300

    adding the bikes part with a slider

---
+++ MX-Arg/src/components/Bike/Bike.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './bike.css';
+
+const Bike = () => {
+  return <div>Bike</div>;
+};
+
+export default Bike;
--- MX-Arg/src/containers/Bikes/Bikes.jsx
+++ MX-Arg/src/containers/Bikes/Bikes.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
 import './bikes.css';
+import Bike from '../../components/Bike/Bike';
+
+import { Navigation, Pagination } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const Bikes = () => {
-  return <div>Bikes</div>;
+  return (
+    <section className='main__bikes section__padding'>
+      <h2>Bikes available</h2>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination]}
+        spaceBetween={50}
+        slidesPerView={1}
+        centeredSlides={true}
+        loop={true}
+        navigation
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+        className='main__bikes-slider'
+      >
+        <SwiperSlide>
+          <Bike img='' title='' />1
+        </SwiperSlide>
+        <SwiperSlide>
+          <Bike img='' title='' />2
+        </SwiperSlide>
+        <SwiperSlide>
+          <Bike img='' title='' />3
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  );
 };
 
 export default Bikes;
