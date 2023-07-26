8c32aa8b305f3de6c4abe4eb2f4b33ef8d9b8b0f
Tomás Arrativel <arrativeltomas@gmail.com>
2023-07-26 10:05:46 -0300

    adding the contact section

--- MX-Arg/src/containers/Contact/Contact.jsx
+++ MX-Arg/src/containers/Contact/Contact.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 import './contact.css';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className='main__contact section__padding' id='contact'>
+      <h2>Contact Us</h2>
+
+      <div className='main__contact-form'>
+        <img src='img/contactimg.png' alt='Image of contact section' />
+        <form>
+          <input type='text' placeholder='First Name' required />
+          <input type='text' placeholder='Last Name' required />
+          <textarea placeholder='Your message here' required></textarea>
+          <button>Submit</button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
