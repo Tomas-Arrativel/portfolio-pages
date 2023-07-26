ec3314689a18050d9680bae564d59e60beb3814c
Tomás Arrativel <arrativeltomas@gmail.com>
2023-07-26 10:16:59 -0300

    add the footer section

--- MX-Arg/src/containers/Footer/Footer.jsx
+++ MX-Arg/src/containers/Footer/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './footer.css';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <p>Copyright &copy; MX ARG, by Tomás Arrativel, 2023</p>
+    </footer>
+  );
 };
 
 export default Footer;
